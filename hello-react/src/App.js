abb5bb5ce686d074224de6fcf8a4f91e4d4a0b33
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-03 18:23:50 +0900

    3.3.1~3.3.6. Functional MyComponent

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import MyComponent from "./MyComponent";
 
 const App = () => {
-    return <MyComponent/>
+    return <MyComponent name="React" favoriteNumber={4}>리액트</MyComponent>
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,23 @@
 import React from "react";
+import PropsTypes from 'prop-types';
 
-const MyComponent = () => {
-    return <div>My awesome new component!</div>;
+const MyComponent = ({name, favoriteNumber, children}) => {
+    return (
+        <div>
+            Hello. My name is {name}. <br />
+            children value is {children}. <br />
+            My favorite number is {favoriteNumber}.
+        </div>
+    );
 }
+
+MyComponent.defaultProps = {
+    name: '기본 이름'
+};
+
+MyComponent.propsTypes = {
+    name: PropsTypes.string,
+    favoriteNumber: PropsTypes.number.isRequired
+};
 
 export default MyComponent;
