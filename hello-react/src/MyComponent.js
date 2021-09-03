765dea3d7c646906febd0be1cd23bbe981250d48
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-03 18:27:13 +0900

    3.3.7. Props with Class MyComponent

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import PropsTypes from 'prop-types';
 
-const MyComponent = ({name, favoriteNumber, children}) => {
-    return (
-        <div>
-            Hello. My name is {name}. <br />
-            children value is {children}. <br />
-            My favorite number is {favoriteNumber}.
-        </div>
-    );
+class MyComponent extends Component {
+    static defaultProps = {
+        name: '기본 이름'
+    };
+
+    static propsTypes = {
+        name: PropsTypes.string,
+        favoriteNumber: PropsTypes.number.isRequired
+    };
+
+    render() {
+        const {name, favoriteNumber, children} = this.props;
+        return (
+            <div>
+                Hello. My name is {name}. <br/>
+                children value is {children}. <br/>
+                My favorite number is {favoriteNumber}.
+            </div>
+        );
+    }
 }
-
-MyComponent.defaultProps = {
-    name: '기본 이름'
-};
-
-MyComponent.propsTypes = {
-    name: PropsTypes.string,
-    favoriteNumber: PropsTypes.number.isRequired
-};
 
 export default MyComponent;
