cb48d62dc0e385e5e6a5c036f63900e8f2512e3e
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-03 18:00:27 +0900

    2.4.6. Inline styling

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 
 function App() {
-    const name = undefined;
-    return <div>{name || 'React'}</div>;
+    const name = 'React';
+
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16
+    };
+
+    return <div style={style}>{name}</div>
 }
 
 export default App;
