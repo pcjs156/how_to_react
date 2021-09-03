ab402ec2cd6f11659fe4167f7ea90d601f5c36a6
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-03 22:11:52 +0900

    5.3. Scrollbar to bottom

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,16 @@
 import {Component} from "react";
 import './App.css';
-import ValidationSample from './ValidationSample'
+import ScrollBox from './ScrollBox'
 
 class App extends Component {
     render() {
         return (
-            <ValidationSample/>
+            <div>
+                <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+                <button onClick={() => this.scrollBox.scrollToBottom()}>
+                    Go To Bottom
+                </button>
+            </div>
         );
     }
 };
