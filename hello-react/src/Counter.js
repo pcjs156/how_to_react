f2ee31fdaf575eda2888fa3c0a8a7c817ba1bd98
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-03 18:42:51 +0900

    3.4.1.1. multiple values in state object

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -5,15 +5,17 @@ class Counter extends Component {
         super(props);
         // Initialize state
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
 
     render() {
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>Immutable Value: {fixedNumber}</h2>
                 <button
                     // Set function that is called when button is clicked with onClick
                     onClick={() => {
@@ -21,7 +23,7 @@ class Counter extends Component {
                         this.setState({number: number + 1});
                     }}
                 >
-                +1
+                    +1
                 </button>
             </div>
         );
