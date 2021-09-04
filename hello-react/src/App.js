44dcac39b1fb125022fb560bc07b5270f93ac205
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-04 15:20:46 +0900

    7.3.2. Lifecycle in App component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,26 @@
 import { Component } from "react";
-import './App.css';
 import LifeCycleSample from './LifeCycleSample'
 
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
+    state = {
+        color: '#000000'
+    };
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    }
+
     render() {
         return (
-            <LifeCycleSample />
+            <div>
+                <button onClick={this.handleClick}>Random Color!</button>
+                <LifeCycleSample color={this.state.color} />
+            </div>
         );
     }
 };
