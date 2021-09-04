88d78a087c0c024dca7915ea86fe2c5e709c03d5
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-04 14:45:19 +0900

    6.4.2. Iteration update

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-    const names = ['Snowman', 'Ice', 'Snow', 'Wind'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    const [names, setNames] = useState([
+        { id: 1, text: 'Snowman' },
+        { id: 2, text: 'Ice' },
+        { id: 3, text: 'Snow' },
+        { id: 4, text: 'Wind' }
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(names.length + 1);
+
+    const onChange = e => setInputText(e.target.value);
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
+
+    const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
     return (
-        <ul>
-            {nameList}
-        </ul>
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            <ul>
+                {nameList}
+            </ul>
+        </>
     )
 };
 
