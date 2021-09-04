387e036edebeb08775e692d99b1480b1895c0efc
Yubin Park <itqkrdbqls1001@naver.com>
2021-09-04 14:35:55 +0900

    6.3. IterationSample with key attribute

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IterationSample = () => {
     const names = ['Snowman', 'Ice', 'Snow', 'Wind'];
-    const nameList = names.map(name => <li>{name}</li>);
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
     return (
         <ul>
             {nameList}
